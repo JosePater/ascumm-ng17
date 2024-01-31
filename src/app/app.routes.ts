93d84262830716e3f1202f14be2dd792b9978bb5
PaterDev <josepater_1130@hotmail.com>
2024-01-30 23:11:36 -0500

    feat(pages): pages restructuring

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [{
-    path:'', component:HomeComponent
-}];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'nosotros', component: AboutUsComponent },
+  { path: 'proyectos', component: ProjectsComponent },
+  { path: 'contacto', component: ContactComponent },
+];
