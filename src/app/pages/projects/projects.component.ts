749520ef924807f98f1813311d236ecc656a8db1
PaterDev <josepater_1130@hotmail.com>
2024-02-01 22:39:47 -0500

    feat(projects): added initial projects page

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -9,6 +9,23 @@ import { Title } from '@angular/platform-browser';
   styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
+  projects = [
+    {
+      name: 'Piscicultura',
+      descrip: 'Proyecto de cachamas',
+      img: 'assets/images/proyectos-cachama.jpg',
+    },
+    {
+      name: 'Cultivo de yuca',
+      descrip: 'Cosecha de yuca dulce harinosa',
+      img: 'assets/images/proyectos-yuca.jpg',
+    },
+    {
+      name: 'Pollos',
+      descrip: 'Pollos purina de engorde',
+      img: 'assets/images/proyectos-pollos.jpg',
+    },
+  ];
   constructor(titulo: Title) {
     titulo.setTitle('Proyectos | ASCUMM');
   }
