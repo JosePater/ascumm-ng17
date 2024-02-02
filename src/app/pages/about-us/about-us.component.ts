60bfc4514776f060b84e8ae402e5bb36b3f2020d
PaterDev <josepater_1130@hotmail.com>
2024-02-01 21:35:22 -0500

    chore(title): added title of about-us page

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
   imports: [],
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.css'
+  styleUrl: './about-us.component.css',
 })
 export class AboutUsComponent {
-
+  constructor(titulo: Title) {
+    titulo.setTitle('Nosotros');
+  }
 }
