07abb4bda2e5206eb79a8800fad134ccb705c01d
PaterDev <josepater_1130@hotmail.com>
2024-02-08 18:13:25 -0500

    feat(contact): added contact page

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -8,8 +8,16 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
-export class ContactComponent {
-  constructor(titulo: Title) {
-    titulo.setTitle('Contacto | ASCUMM');
+export class ContactComponent implements OnInit {
+  private _titulo: Title = inject(Title);
+
+  contact = {
+    email: 'ascumsampues@gmail.com',
+    address: 'Calle 15E carrera 18C - 9 Urb Geney',
+    phone: ['314 7016179', '314 5508626', '320 3155111'],
+  };
+
+  ngOnInit(): void {
+    this._titulo.setTitle('Contacto | ASCUMM');
   }
 }
